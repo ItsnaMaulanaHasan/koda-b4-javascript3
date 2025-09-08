7cfca833bd31d95e18233c32cd55a944a0e9aa27
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-09-08 14:35:59 +0700

    format code

--- minitask3.js
+++ minitask3.js
@@ -1,42 +1,48 @@
 const wait = (str, time) => {
-    return new Promise((resolve, reject) => {
-        if (typeof str !== "string"){
-            reject("Tipe data tidak sesuai")
-        } else {
-            setTimeout(() => {
-                resolve(str)
-            }, time)
-        }
-    })
-}
+  return new Promise((resolve, reject) => {
+    if (typeof str !== "string") {
+      reject("Tipe data tidak sesuai");
+    } else {
+      setTimeout(() => {
+        resolve(str);
+      }, time);
+    }
+  });
+};
 
-wait("John", 1500).then((res) => {
-    console.log(res)
-    wait("Ed", 2000).then((res) => {
-        console.log(res)
-        wait("Jane", 500).then((res) => {
-            console.log(res)
-        }).catch((err) => {
-            console.log(err)
-        })
-    }).catch((err) => {
-        console.log(err)
-    })
-}).catch((err) => {
-    console.log(err)
-})
+wait("John", 1500)
+  .then((res) => {
+    console.log(res);
+    wait("Ed", 2000)
+      .then((res) => {
+        console.log(res);
+        wait("Jane", 500)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    try {
-        const ress1 = await wait("John", 1500)
-        console.log(ress1)
-        const ress2 = await wait("Ed", 2000)
-        console.log(ress2)
-        const ress3 = await wait("Jane", 500)
-        console.log(ress3)
-    } catch (err) {
-        console.log(err)
-    }
-} 
+  try {
+    const ress1 = await wait("John", 1500);
+    console.log(ress1);
+    const ress2 = await wait("Ed", 2000);
+    console.log(ress2);
+    const ress3 = await wait("Jane", 500);
+    console.log(ress3);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 // main()

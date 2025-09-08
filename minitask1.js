a019f5b93091398497d6b4f9a2bc3fe35030aa0e
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-09-08 09:44:34 +0700

    add more examples

--- minitask1.js
+++ minitask1.js
@@ -19,3 +19,28 @@ console.log(biodata)
 // Built-in Method
 Object.assign(biodata, biodataExtra2)
 console.log(biodata)
+
+const arr = ["Itsna", "Maulana", "Hasan", 23, "Pati"]
+
+// Manual
+const search = "Itsna"
+let found = false
+for (let i = 0; i < arr.length; i++){
+    if(arr[i] === search){
+        found = true
+    }
+}
+
+if(found){
+    console.log("Ditemukan")
+} else {
+    console.log("Tidak ditemukan")
+}
+
+// Built-in Method
+let isFound = arr.includes("Halo")
+if(isFound){
+    console.log("Ditemukan")
+} else {
+    console.log("Tidak ditemukan")
+}
